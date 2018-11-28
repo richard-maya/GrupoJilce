3c481203560599abc24f5fe821c8505843200a42
Richard Maya <richard.maya94@gmail.com>
2018-11-28 16:18:05 -0600

    Initial commit

---
+++ assets/js/jilce.js
@@ -0,0 +1,54 @@
+// HEADER
+// ------------------------------->
+$(document).on("scroll", function () {
+    "use strict";
+    if ($(document).scrollTop() > 50) {
+        // console.log("Ya bajamos");
+        $(".navbar").addClass("navbar-small");
+    } else {
+        $(".navbar").removeClass("navbar-small");
+    }
+});
+
+
+// SMOOTH SCROLL
+// ------------------------------->
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000,
+    easing: 'easeInOutQuint'
+});
+
+
+// WOW JS
+// ------------------------------->
+var wow = new WOW({
+    offset: -20,
+    mobile: false
+});
+
+
+// DOCUMENT READY
+// ------------------------------->
+$(document).ready(function () {
+    "use strict";
+    wow.init();
+    $("p:not(.success-message)").addClass("wow fadeInUp");
+    $("p.success-message").addClass("wow zoomIn");
+    $("h6").addClass("wow fadeInUp");
+    $("h5").addClass("wow fadeInUp");
+    $("h4").addClass("wow fadeInUp");
+    $("h3").addClass("wow fadeInUp");
+    $("h2").addClass("wow fadeInUp");
+    $("h1").addClass("wow fadeInUp");
+    $("button.btn-white-outline").addClass("wow fadeInUp");
+    $("form").addClass("wow fadeInLeft");
+    $("li:not(.nav-item)").addClass("wow fadeInUp");
+    $("i.fa-facebook-square").addClass("wow fadeInUp");
+    $("i.fa-twitter").addClass("wow fadeInUp");
+
+    var link = document.location.search;
+
+    if (link.includes("success")) {
+        $('#message-modal').modal('show');
+    }
+});
